5a26e956f091f30e46149c83ebb93e922b89b460
Vladimir <scobca18@yandex.ru>
2025-02-19 01:10:56 +0300

    feat(user): created user's personal account

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,12 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HelloWorld from "../components/HelloWorld.vue";
 import ProfessionsList from "../pages/ProfessionsList.vue";
+import UserPersonalAccount from "../pages/UserPersonalAccount.vue";
 
 const routes = [
   { path: '/', component: HelloWorld },
   { path: '/professions', component: ProfessionsList },
+  { path: '/user', component: UserPersonalAccount },
 ];
 
 const router = createRouter({
