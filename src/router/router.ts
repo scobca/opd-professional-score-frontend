9041c950e5c1a5469a31c11e4b64d3880a757bf7
Vladimir <scobca18@yandex.ru>
2025-02-19 03:36:06 +0300

    feat(admin): created admins personal account

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,12 +3,14 @@ import HelloWorld from "../components/HelloWorld.vue";
 import ProfessionsList from "../pages/ProfessionsList.vue";
 import UserPersonalAccount from "../pages/UserPersonalAccount.vue";
 import ExpertPersonalAccount from "../pages/ExpertPersonalAccount.vue";
+import AdminPersonalAccount from "../pages/AdminPersonalAccount.vue";
 
 const routes = [
   { path: '/', component: HelloWorld },
   { path: '/professions', component: ProfessionsList },
   { path: '/user', component: UserPersonalAccount },
   { path: '/expert', component: ExpertPersonalAccount },
+  { path: '/admin', component: AdminPersonalAccount },
 ];
 
 const router = createRouter({
