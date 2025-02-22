bd8198d5759ae2d91c35657a733e8f831271076a
TheGeniusOfEternity <max06safin@yandex.ru>
2025-02-22 23:28:26 +0300

    feat(app) sending requests to update user's roles

--- js/admin.js
+++ js/admin.js
@@ -1,20 +1,19 @@
 import {getCookie} from "./cookieHandler.js";
 import {dataFromJWT} from "./jwtHandler.js";
+import {popupHandler} from "./popup.js";
+
 const nextBtn = document.getElementById("nextBtn");
 const prevBtn = document.getElementById("prevBtn");
-const changeBtn = document.getElementById("changeBtn");
 const usersTable = document.querySelector(".data");
+const modal = document.querySelector(".role-select");
+const modalForm = document.getElementById("role-change-form");
 let page = 1
-let user = null
+const user = dataFromJWT(getCookie("jwt")).data;
 let renderedRows = []
 const loadUsers = async() => {
-    const jwt = getCookie("jwt")
-    if (jwt) {
-        user = dataFromJWT(jwt).data
-        if (user.role === "admin") {
-            const response = await fetch("http://localhost:8081/get-users-all")
-            return await response.json()
-        }
+    if (user.role === "admin") {
+        const response = await fetch("http://localhost:8081/get-users-all")
+        return await response.json()
     } else {
         window.location.replace("sign-in.html")
     }
@@ -29,27 +28,46 @@ const renderUsers = async (page) => {
     }
 
     for (let i = (page - 1) * 5; i < (page) * 5; i++) {
-        const userRow = document.createElement('div')
-        userRow.classList.add('userRow')
-        const idDiv = document.createElement('div')
-        const usernameDiv = document.createElement('div')
-        const emailDiv = document.createElement('div')
-        const roleDiv = document.createElement('div')
-        idDiv.classList.add('id')
-        usernameDiv.classList.add('username')
-        emailDiv.classList.add('email')
-        roleDiv.classList.add('role')
-        idDiv.innerHTML = users[i].id
-        usernameDiv.innerHTML = users[i].username
-        emailDiv.innerHTML = users[i].email
-        roleDiv.innerHTML = users[i].role
-        userRow.append(idDiv, usernameDiv, emailDiv, roleDiv)
-        if (users[i].email !== user.email) {
-            const changeRole = document.createElement('button')
-            userRow.append(changeRole)
+        if (users[i]) {
+            const userRow = document.createElement('div')
+            userRow.classList.add('userRow')
+            const idDiv = document.createElement('div')
+            const usernameDiv = document.createElement('div')
+            const emailDiv = document.createElement('div')
+            const roleDiv = document.createElement('div')
+            idDiv.classList.add('id')
+            usernameDiv.classList.add('username')
+            emailDiv.classList.add('email')
+            roleDiv.classList.add('role')
+            idDiv.innerHTML = users[i].id
+            usernameDiv.innerHTML = users[i].username
+            emailDiv.innerHTML = users[i].email
+            roleDiv.innerHTML = users[i].role
+            userRow.append(idDiv, usernameDiv, emailDiv, roleDiv)
+            if (users[i].email !== user.email) {
+                const changeRole = document.createElement('button')
+                changeRole.innerHTML = "Изменить роль"
+                userRow.append(changeRole)
+
+                changeRole.addEventListener("click", () => {
+                    Array.from(modalForm.elements).forEach(input => {
+                        if (input.value === users[i].role) {
+                            input.checked = true
+                        } else if (input.type === "number") {
+                            input.value = users[i].id
+                        }
+                    })
+                    modal.classList.toggle("hide")
+                    const btnCoords = changeRole.getBoundingClientRect()
+                    const modalCoords = modal.getBoundingClientRect()
+                    modal.style.top = btnCoords.top + btnCoords.height + 15 + 'px'
+                    modal.style.left = btnCoords.left - modalCoords.width + btnCoords.width + "px"
+                    modal.children[0].innerHTML = `Изменить роль пользователя ${users[i].username}`
+                })
+            }
+            usersTable.appendChild(userRow)
+            renderedRows.push(userRow)
         }
-        usersTable.appendChild(userRow)
-        renderedRows.push(userRow)
     }
 }
 
@@ -66,7 +84,28 @@ nextBtn.addEventListener("click", (e) => {
     }
 })
 
-
 window.addEventListener("load", () => {
     renderUsers(1)
 });
+
+modalForm.onsubmit = async (e) => {
+    const userId = Array.from(modalForm.elements).find(input => input.type === "number").value
+    const newRole = Array.from(modalForm.elements).find(input => input.checked === true).value
+    const data = new FormData
+    data.append("user_id", userId)
+    data.append("admin_email", user.email)
+    data.append("new_role", newRole)
+    e.preventDefault()
+    const response = await fetch("http://localhost:8081/update-role", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${getCookie("jwt")}`,
+        },
+        body: data
+    })
+    const result = await response.json()
+    popupHandler(result)
+    if (result.status === 200) {
+        renderUsers(page)
+    }
+}
