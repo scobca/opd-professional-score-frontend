a84ca79ee7c758197153cc2aa59f3953338e7a08
m4shustik-s <m4shustik.s@yandex.ru>
2025-02-26 22:21:06 +0300

    added a profile-update file
    you can change the photo (if it's inconvenient to add to the profile page, we'll remove it) and username

---
+++ scriptph.js
@@ -0,0 +1,15 @@
+function previewImage(event) {
+    const file = event.target.files[0]; 
+    const preview = document.getElementById('preview'); 
+    if (file) {
+        const reader = new FileReader(); 
+        reader.onload = function(e) {
+            preview.src = e.target.result; 
+            preview.style.display = 'block'; 
+        }
+        reader.readAsDataURL(file); 
+    } 
+    else {
+        preview.style.display = 'none'; 
+    }
+}
