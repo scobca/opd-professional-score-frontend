39a487fda7426fe48d6f4ee034bf7d7d0c597ab2
TheGeniusOfEternity <max06safin@yandex.ru>
2025-02-28 01:39:21 +0300

    feat(app): get professions from server
    Now all professions (except first 3 templates) are got from server and could be shown in main page as cards, or in profile page (for admin) as list

--- js/admin.js
+++ js/admin.js
@@ -2,15 +2,38 @@ import {getCookie} from "./cookieHandler.js";
 import {dataFromJWT} from "./jwtHandler.js";
 import {popupHandler} from "./popup.js";
 
-const nextBtn = document.getElementById("nextBtn");
-const prevBtn = document.getElementById("prevBtn");
-const usersTable = document.querySelector(".data");
+const nextBtnUsers = document.getElementById("nextBtnUsers");
+const nextBtnProfessions = document.getElementById("prevBtnProfessions");
+const prevBtnUsers = document.getElementById("prevBtnUsers");
+const prevBtnProfessions = document.getElementById("prevBtnProfessions");
+const usersTable = document.querySelector(".userData");
+const professionsTable = document.querySelector(".professionData");
 const modal = document.querySelector(".role-select");
 const modalForm = document.getElementById("role-change-form");
-let page = 1
+let userPage = 1
+let professionPage = 1
 const user = dataFromJWT(getCookie("jwt")).data;
-let renderedRows = []
+let renderedUserRows = []
+let renderedProfessionRows = []
 const loadUsers = async() => {
+    if (user.role === "admin") {
+        const response = await fetch("http://localhost:8081/get-users-all", {
+            headers: {
+                Authorization: `Bearer ${getCookie("jwt")}`,
+            },
+        })
+        return await response.json()
+    } else {
+        window.location.replace("sign-in.html")
+    }
+}
+
+const loadProfessions = async() => {
+    const response = await fetch("http://localhost:8081/get-professions-all")
+    return await response.json()
+}
+
+const loadTests = async() => {
     if (user.role === "admin") {
         const response = await fetch("http://localhost:8081/get-users-all")
         return await response.json()
@@ -19,15 +42,15 @@ const loadUsers = async() => {
     }
 }
 
-const renderUsers = async (page) => {
+const renderUsers = async (userPage) => {
     const users = await loadUsers()
-    if (renderedRows) {
-        renderedRows.forEach(row => {
+    if (renderedUserRows) {
+        renderedUserRows.forEach(row => {
             row.remove()
         })
     }
 
-    for (let i = (page - 1) * 5; i < (page) * 5; i++) {
+    for (let i = (userPage - 1) * 5; i < (userPage) * 5; i++) {
         if (users[i]) {
             const userRow = document.createElement('div')
             userRow.classList.add('userRow')
@@ -66,26 +89,86 @@ const renderUsers = async (page) => {
                 })
             }
             usersTable.appendChild(userRow)
-            renderedRows.push(userRow)
+            renderedUserRows.push(userRow)
         }
     }
 }
 
-prevBtn.addEventListener("click", (e) => {
-    if (page > 1) {
-        page -= 1
-        renderUsers(page)
+const renderProfessions = async (professionPage) => {
+    const professions = await loadProfessions()
+    if (renderedProfessionRows) {
+        renderedProfessionRows.forEach(row => {
+            row.remove()
+        })
+    }
+    for (let i = (professionPage - 1) * 5; i < (professionPage) * 5; i++) {
+        if (professions.data[i]) {
+            const professionRow = document.createElement('div')
+            professionRow.classList.add('professionRow')
+            const idDiv = document.createElement('div')
+            const nameDiv = document.createElement('div')
+            const descDiv = document.createElement('div')
+            idDiv.classList.add('id')
+            nameDiv.classList.add('username')
+            descDiv.classList.add('email')
+            idDiv.innerHTML = professions.data[i].id
+            nameDiv.innerHTML = professions.data[i].name
+            descDiv.innerHTML = professions.data[i].description
+            professionRow.append(idDiv, nameDiv, descDiv)
+            // const changeRole = document.createElement('button')
+            // changeRole.innerHTML = "Изменить роль"
+            // userRow.append(changeRole)
+            //
+            // changeRole.addEventListener("click", () => {
+            //     Array.from(modalForm.elements).forEach(input => {
+            //         if (input.value === users[i].role) {
+            //             input.checked = true
+            //         } else if (input.type === "number") {
+            //             input.value = users[i].id
+            //         }
+            //     })
+            //     modal.classList.toggle("hide")
+            //     const btnCoords = changeRole.getBoundingClientRect()
+            //     const modalCoords = modal.getBoundingClientRect()
+            //     modal.style.top = btnCoords.top + btnCoords.height + 15 + 'px'
+            //     modal.style.left = btnCoords.left - modalCoords.width + btnCoords.width + "px"
+            //     modal.children[0].innerHTML = `Изменить роль пользователя ${users[i].username}`
+            // })
+            professionsTable.appendChild(professionRow)
+            renderedProfessionRows.push(professionRow)
+        }
+    }
+}
+
+prevBtnUsers.addEventListener("click", async (e) => {
+    if (userPage > 1) {
+        userPage -= 1
+        await renderUsers(userPage)
     }
 })
-nextBtn.addEventListener("click", (e) => {
-    if (page * 5 < loadUsers().length) {
-        page += 1
-        renderUsers(page)
+nextBtnUsers.addEventListener("click", async (e) => {
+    if (userPage * 5 < loadUsers().length) {
+        userPage += 1
+        await renderUsers(userPage)
     }
 })
 
-window.addEventListener("load", () => {
-    renderUsers(1)
+prevBtnProfessions.addEventListener("click", async (e) => {
+    if (professionPage > 1) {
+        professionPage -= 1
+        await renderProfessions(professionPage)
+    }
+})
+nextBtnProfessions.addEventListener("click", async (e) => {
+    if (professionPage * 5 < loadUsers().length) {
+        professionPage += 1
+        await renderProfessions(professionPage)
+    }
+})
+
+window.addEventListener("load", async () => {
+    await renderUsers(1)
+    await renderProfessions(1)
 });
 
 modalForm.onsubmit = async (e) => {
@@ -106,6 +189,6 @@ modalForm.onsubmit = async (e) => {
     const result = await response.json()
     popupHandler(result)
     if (result.status === 200) {
-        renderUsers(page)
+        await renderUsers(userPage)
     }
 }
