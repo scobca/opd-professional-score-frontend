f64358828a4ef8b6cfd8f004ddd9268cdf12f1ad
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-18 00:49:05 +0300

    docs(socket): created example how to connect socket

---
+++ src/components/SocketTest.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import {defineComponent, inject} from 'vue'
+import {io} from "socket.io-client";
+import CustomInput from "./UI/inputs/CustomInput.vue";
+
+export default defineComponent({
+  name: "SocketTest",
+  components: {CustomInput},
+  data() {
+    return {
+      socket: inject("socket") as ReturnType<typeof io>,
+      inputValue: ""
+    }
+  },
+  mounted() {
+    this.socket.connect();
+  },
+  created() {
+    this.socket.on('searchResults', (data: any) => {
+      console.log('Результаты:', data)
+    })
+  },
+  methods: {
+    search(query: string) {
+      this.socket.emit('search', query)
+    }
+  },
+  unmounted() {
+    this.socket.disconnect();
+  }
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <CustomInput @search="search(inputValue)" v-model="inputValue" :placeholder="'Введите сюда название ПВК'"/>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  height: 2rem;
+}
+</style>
