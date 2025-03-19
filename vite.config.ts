683e2d48952c4d529d471c72f9161423c0c2b0b4
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-19 15:15:33 +0300

    build(core): removed unused libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {createHtmlPlugin} from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    createHtmlPlugin({
-      template: 'src/index.html',
-    })
   ],
 })
