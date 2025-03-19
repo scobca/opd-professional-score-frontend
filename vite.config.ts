d0bf7baa3a3d05d21a569ccb038220b16d576834
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-19 12:59:42 +0300

    feat(app): updated basic tab name & image

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {createHtmlPlugin} from "vite-plugin-html";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      template: 'src/index.html',
+    })
+  ],
 })
